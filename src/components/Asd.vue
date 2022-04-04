4745b0cf03f47df6b177cf867efa6c78b5091320
Kristall01 <14259961+Kristall01@users.noreply.github.com>
2022-04-04 09:46:10 +0200

    initial

---
+++ src/components/Asd.vue
@@ -0,0 +1,16 @@
+<template>
+	<form @submit="asd">
+		<input type="submit">
+	</form>
+</template>
+
+<script>
+	export default {
+		methods: {
+			asd(e) {
+				e.preventDefault();
+				console.log("hello");
+			}
+		}
+	}
+</script>
